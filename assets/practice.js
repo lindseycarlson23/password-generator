869d7ae91537906973e6b3cac6330e2a3d2bedc0
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-09-27 21:36:01 -0500

    added additional pseudo code

--- assets/practice.js
+++ assets/practice.js
@@ -19,10 +19,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // array for each character type
-var numArray = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = [" ", "!", """, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var numArray = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specialChar = [" ", "!", """, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 function generatePassword(){
   var numChar = window.prompt("How many characters would you like your password to contain?");
